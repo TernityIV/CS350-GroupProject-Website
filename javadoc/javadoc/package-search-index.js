2d29dd59a6a9ea65030d7565d27a64d11f42f5ef
Project Reporting Actions <ci@email.address>
2022-11-26 00:44:31 +0000

    Updating-webpages

--- javadoc/javadoc/package-search-index.js
+++ javadoc/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.odu.cs.cs350.acmClassifier"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.odu.cs.cs350.acmClassifier"}]
